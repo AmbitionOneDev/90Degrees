26055210771d6747dd4848f73854a7393191d4fb
ZPetricusic <chromaa19@gmail.com>
2020-12-26 21:17:18 +0100

    test message

--- Scripts/BuzzsawRotation.cs
+++ Scripts/BuzzsawRotation.cs
@@ -3,6 +3,7 @@
 public class BuzzsawRotation : MonoBehaviour
 {
     private float rotationSpeed = 300f;
+
     public void FixedUpdate()
     {
         transform.Rotate(new Vector3(0f, 0f, rotationSpeed) * Time.fixedDeltaTime);    
